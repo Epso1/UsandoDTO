bef59cf5200dc86420a6fb01ae565eb51a0509ed
Epso1 <cesaralvarorubert@gmail.com>
2024-01-11 18:52:23 +0100

    Primer Commit

--- src/main/java/com/example/mi_primera_api_rest/repository/DriverRepository.java
+++ src/main/java/com/example/mi_primera_api_rest/repository/DriverRepository.java
@@ -3,5 +3,9 @@ package com.example.mi_primera_api_rest.repository;
 import com.example.mi_primera_api_rest.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+   Optional<Driver> findByCodeIgnoreCase(String code);
+
 }
